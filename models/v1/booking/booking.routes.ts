c40db20d8cd72ce08fd8dc7fd3310ded3ab7e891
Tariq-Monowar <hassainseam@gmail.com>
2025-06-23 08:56:43 +0600

    migrate

--- models/v1/booking/booking.routes.ts
+++ models/v1/booking/booking.routes.ts
@@ -1,19 +1,39 @@
+// import { Router } from 'express';
+// import { verifyUser } from '../../../middleware/verifyUsers';
+// import { getAvailableExperts, createBooking, confirmPayment, getMyBookings, cancelBooking } from "./booking.controllers"
+
+// const router = Router();
+
+// router.get("/experts/available", getAvailableExperts)
+
+// router.post("/create", verifyUser("STUDENT"), createBooking)
+
+// router.post("/confirm-payment", confirmPayment)
+
+// router.get("/my-bookings", verifyUser("ANY"), getMyBookings)
+
+// router.put("/cancel/:bookingId", verifyUser("ANY"), cancelBooking)
+
+
+
+// export default router;
+
+
+
+
+
+
+
+
+
+
 import { Router } from 'express';
 import { verifyUser } from '../../../middleware/verifyUsers';
-import { getAvailableExperts, createBooking, confirmPayment, getMyBookings, cancelBooking } from "./booking.controllers"
+import { createBooking, confirmPayment } from "./booking.controllers";
 
 const router = Router();
 
-router.get("/experts/available", getAvailableExperts)
-
-router.post("/create", verifyUser("STUDENT"), createBooking)
-
-router.post("/confirm-payment", confirmPayment)
-
-router.get("/my-bookings", verifyUser("ANY"), getMyBookings)
-
-router.put("/cancel/:bookingId", verifyUser("ANY"), cancelBooking)
-
-
+router.post("/create", verifyUser("STUDENT"), createBooking);
+router.post("/confirm-payment", confirmPayment);
 
 export default router;
